9e16ce3d15b586666a971b570704f3a78f5040d6
River Kahrhoff <river.kahrhoff@gmail.com>
2024-07-16 17:30:50 -0500

    scrabble

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -33,26 +33,94 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   word = input.question("Let's play some scrabble! Enter a word: ")
+   return word
 };
 
-let newPointStructure;
+let newPointStructure = transform(oldPointStructure);
 
-let simpleScorer;
+let simpleScorer = function(word) {
+   return word.length
+};
 
-let vowelBonusScorer;
 
-let scrabbleScorer;
+let vowelBonusScorer = function(word) {
+   let vowels = ["a", "e", "i", "o", "u"]
+   let score = 0
+   for (i = 0; i < word.length; i++) {
+      if (vowels.includes(word[i])) {
+         score = score + 3
+      } else {
+         score = score + 1
+      }
+   }
+   return score
+}
 
-const scoringAlgorithms = [];
+let scrabbleScorer = function(word) {
+  word = word.toLowerCase();
+	let letterPoints = 0;
+	for (let i = 0; i < word.length; i++) {
+	  for (letter in newPointStructure) {
+		 if (letter == (word[i])) {
+			letterPoints += newPointStructure[letter]
+		 }
+ 
+	  }
+	}
+	return letterPoints;
+};
 
-function scorerPrompt() {}
+let scoringAlgorithms = [
+  {
+   name: "Simple Score",
+   description: "Each letter is worth 1 point.",
+   scoringFunction: simpleScorer
+ },
+ {
+   name: "Bonus Vowels",
+   description: "Vowels are 3 pts, consonants are 1 pt.",
+   scoringFunction: vowelBonusScorer
+ },
+ {
+   name: "Scrabble",
+   description: "The traditional scoring algorithm.",
+   scoringFunction: scrabbleScorer
+ }];
+ 
+ function scorerPrompt() {
+   scorerIndex = input.question(`
+ Which scoring algorithm would you like to use?
+ 
+ 0 - Simple: One point per character
+ 1 - Vowel Bonus: Vowels are worth 3 points
+ 2 - Scrabble: Uses scrabble point system
+ Enter 0, 1, or 2: `)
+   if (scorerIndex < 0 || scorerIndex > 3) {
+     console.log("\nERROR: invalid input");
+     scorerPrompt();
+   } else {
+     return console.log(`\nScore for "${word}': ${scoringAlgorithms[scorerIndex].scoringFunction(word)}`);
+   }
+ }
+ 
 
-function transform() {};
+function transform(pointStruct) {
+  let newPointStruct = {};
+  newPointStruct[" "] = 0;
+  for (pointValue in pointStruct) {
+    for (let i = 0; i < pointStruct[pointValue].length; i++) {
+      let newKey = String(pointStruct[pointValue][i].toLowerCase());
+      newPointStruct[newKey] = Number(pointValue);
+    }
+  }
+  return newPointStruct;
+}
+
 
 function runProgram() {
    initialPrompt();
-   
+   scorerPrompt();
 }
 
 // Don't write any code below this line //
